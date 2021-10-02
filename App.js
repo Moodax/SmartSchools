84cf38369c42bf2d5efb0cbd7005c68db426b31d
NSkeva <nikola.skeva1@gmail.com>
2021-10-02 14:51:14 +0200

    s

--- App.js
+++ App.js
@@ -1,6 +1,14 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import Parse from "parse/react-native.js";
+import AsyncStorage from '@react-native-async-storage/async-storage';
+
+//Initializing the SDK. 
+Parse.setAsyncStorage(AsyncStorage);
+//You need to copy BOTH the the Application ID and the Javascript Key from: Dashboard->App Settings->Security & Keys 
+Parse.initialize('YibgIw1eEWcVwyt1I4JOnAUvql4zNmKN6DOcTStf','DSIbUE00btcjKIDbWfUtRc0Z2EmplJxAXviLTX9k');
+Parse.serverURL = 'https://parseapi.back4app.com/';
 
 export default function App() {
   return (
--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,5 @@
-module.exports = function(api) {
-  api.cache(true);
-  return {
-    presets: ['babel-preset-expo'],
-  };
-};
+module.exports = {
+  applicationId:  'YibgIw1eEWcVwyt1I4JOnAUvql4zNmKN6DOcTStf',
+  javascriptKey:  'ExkzX1CNroa1VpERuFSgwKfmXKvYbn9GJeWKZQMe',
+  serverURL:  'https://parseapi.back4app.com/'
+}
