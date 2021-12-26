91f9a6037cb8b61c3ee8a775a5690df6c6cb1dd5
Moodax <64415905+Moodax@users.noreply.github.com>
2021-12-26 11:48:18 +0100

    Verzija 0.004
    Napravljeno povezivanje na server te login

--- navigation/login.js
+++ navigation/login.js
@@ -1,23 +1,75 @@
-import React,{useState} from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import React,{useState,Component} from 'react';
 import { Text, View, StyleSheet, TextInput,FlatList,Button } from 'react-native';
+import { withNavigation } from 'react-navigation';
 
+class Login extends Component {
+  constructor(props){
+    super(props)
+    this.state={
+      username:'',
+      password:''
+    }
+    
+  }
 
-export default function Login({ navigation }) {
+  userLogin=()=>{
+    
+    const{username}=this.state;
+    const{password}=this.state;
+    
+    fetch('http://localhost/connection.php',{
+    method:'post',
+    header:{
+      'Accept':'application/json',
+      'Content-type':'application/json'
+    },
+    body:JSON.stringify({
+      username:username,
+      password:password
+    })
+  }) 
+    .then(response => response.json())
+      .then(responseJson=>{
+        if(responseJson=='1'){
+          this.props.navigation.navigate('MainMenu')
+        }
+        else
+        alert(responseJson);
+      })
+      .catch((error)=>{
+        console.error(error);
+      });
+
+  }
+render(){
   return (
     <View style={styles.container}>
-    <Text style={styles.text}>Username:</Text>
-      <TextInput style={styles.input}></TextInput>
-      <Text style={styles.text}>Password:</Text>
-      <TextInput secureTextEntry='true'
 
-      style={styles.input}></TextInput>
+    <Text style={styles.text}>Username:</Text>
+
+      <TextInput style={styles.input} 
+        onChangeText={(username) => this.setState({ username })}
+      />
+
+      <Text style={styles.text}>Password:</Text>
+
+      <TextInput secureTextEntry='true'
+      style={styles.input}
+      onChangeText={(password) => this.setState({ password })}
+      />
+
       <Button
       title="Login"
-      onPress={() => navigation.navigate('MainMenu')}
+      onPress={this.userLogin}
       />
     </View>
   );
 }
+}
+
+export default withNavigation(Login);
+
 const styles = StyleSheet.create({
     container: {
       flex: 1,
